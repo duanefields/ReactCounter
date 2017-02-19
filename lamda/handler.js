6cd4e329e111a2cafbdb538c033c2527ec507da3
Duane Fields <dfields@glgroup.com>
2017-02-19 09:50:34 -0600

    fix typo

--- lamda/handler.js
+++ lamda/handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       date: new Date(),
