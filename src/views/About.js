2103aeff8bdba5a3ffc0d484b56899a0e971130f
Duane Fields <duane@Duanes-MacBook-Pro.local>
2017-02-20 16:24:32 -0600

    Basic CSS Modules

--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from './About.css'
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>About This App</h1>
+        <h1 className={styles.blue}>About This App</h1>
         <p>Just a test...</p>
       </div>
     );
--- src/views/Counter.js
+++ src/views/Counter.js
@@ -6,6 +6,7 @@ export default class Counter extends Component {
   render() {
     return (
       <div>
+        <h1 className="blue">The Counter</h1>
         <ClickList store={ClickStore} />
       </div>
     );
