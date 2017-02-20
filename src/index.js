fdc5c04bcfa8365191c73b67162ca86887ccc0d1
Duane Fields <duane@Duanes-MacBook-Pro.local>
2017-02-20 10:41:11 -0600

    Add index route

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App, Counter, About } from './views';
 import './styles/index.styl';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router'
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRedirect to="/counter"/>
       <Route path="/counter" component={Counter}/>
       <Route path="/about" component={About}/>
     </Route>
